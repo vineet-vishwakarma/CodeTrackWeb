a18312b75c5d74ea26fdfd31b94132d1d811cb39
vineet22vishwakarma@gmail.com <vineet22vishwakarma@gmail.com>
2024-07-06 17:15:10 +0530

    add badges section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1523069424"
+    serviceWorkerVersion: "2083410709"
   }
 });
